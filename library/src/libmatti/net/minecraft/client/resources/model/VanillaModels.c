7c2ee5e973ee01df28ac462e80a6a8f544700b02
Matthias Plösch <mattiasploesch@users.noreply.github.com>
2026-09-22 12:33:47 +0200

    Add VanillaModels: embedded cube_all/cube/stone/dirt model JSONs (1:1 assets)

---
+++ library/src/libmatti/net/minecraft/client/resources/model/VanillaModels.c
@@ -0,0 +1,96 @@
+// The embedded vanilla block models (implementation). The JSONs mirror the
+// vanilla asset files 1:1 (cube_all, cube, stone, dirt).
+
+#include "libmatti/net/minecraft/client/resources/model/VanillaModels.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+// Java: assets/minecraft/models/block/cube_all.json
+const char *const LIBMATTI_MC_VanillaModels_CUBE_ALL =
+    "{\n"
+    "    \"parent\": \"block/block\",\n"
+    "    \"render_type\": \"minecraft:solid\",\n"
+    "    \"textures\": {\n"
+    "        \"all\": \"#missing\",\n"
+    "        \"particle\": \"#all\"\n"
+    "    },\n"
+    "    \"elements\": [\n"
+    "        {   \"from\": [ 0, 0, 0 ],\n"
+    "            \"to\": [ 16, 16, 16 ],\n"
+    "            \"faces\": {\n"
+    "                \"down\":  { \"texture\": \"#all\", \"cullface\": \"down\" },\n"
+    "                \"up\":    { \"texture\": \"#all\", \"cullface\": \"up\" },\n"
+    "                \"north\": { \"texture\": \"#all\", \"cullface\": \"north\" },\n"
+    "                \"south\": { \"texture\": \"#all\", \"cullface\": \"south\" },\n"
+    "                \"west\":  { \"texture\": \"#all\", \"cullface\": \"west\" },\n"
+    "                \"east\":  { \"texture\": \"#all\", \"cullface\": \"east\" }\n"
+    "            }\n"
+    "        }\n"
+    "    ]\n"
+    "}\n";
+
+// Java: assets/minecraft/models/block/cube.json
+const char *const LIBMATTI_MC_VanillaModels_CUBE =
+    "{\n"
+    "    \"parent\": \"block/block\",\n"
+    "    \"render_type\": \"minecraft:solid\",\n"
+    "    \"textures\": {\n"
+    "        \"particle\": \"#north\",\n"
+    "        \"north\": \"#north\",\n"
+    "        \"south\": \"#south\",\n"
+    "        \"east\": \"#east\",\n"
+    "        \"west\": \"#west\",\n"
+    "        \"up\": \"#up\",\n"
+    "        \"down\": \"#down\"\n"
+    "    },\n"
+    "    \"elements\": [\n"
+    "        {   \"from\": [ 0, 0, 0 ],\n"
+    "            \"to\": [ 16, 16, 16 ],\n"
+    "            \"faces\": {\n"
+    "                \"down\":  { \"texture\": \"#down\", \"cullface\": \"down\" },\n"
+    "                \"up\":    { \"texture\": \"#up\", \"cullface\": \"up\" },\n"
+    "                \"north\": { \"texture\": \"#north\", \"cullface\": \"north\" },\n"
+    "                \"south\": { \"texture\": \"#south\", \"cullface\": \"south\" },\n"
+    "                \"west\":  { \"texture\": \"#west\", \"cullface\": \"west\" },\n"
+    "                \"east\":  { \"texture\": \"#east\", \"cullface\": \"east\" }\n"
+    "            }\n"
+    "        }\n"
+    "    ]\n"
+    "}\n";
+
+// Java: assets/minecraft/models/block/stone.json
+const char *const LIBMATTI_MC_VanillaModels_STONE =
+    "{\n"
+    "    \"parent\": \"block/cube_all\",\n"
+    "    \"textures\": {\n"
+    "        \"all\": \"block/stone\"\n"
+    "    }\n"
+    "}\n";
+
+// Java: assets/minecraft/models/block/dirt.json
+const char *const LIBMATTI_MC_VanillaModels_DIRT =
+    "{\n"
+    "    \"parent\": \"block/cube_all\",\n"
+    "    \"textures\": {\n"
+    "        \"all\": \"block/dirt\"\n"
+    "    }\n"
+    "}\n";
+
+const char *const LIBMATTI_MC_VanillaModels_STONE_ID = "minecraft:block/stone";
+const char *const LIBMATTI_MC_VanillaModels_DIRT_ID = "minecraft:block/dirt";
+
+const char *LIBMATTI_MC_VanillaModels_ModelById(const char *modelId)
+{
+    if (modelId == NULL)
+        return NULL;
+    if (strcmp(modelId, "minecraft:block/cube_all") == 0 || strcmp(modelId, "block/cube_all") == 0)
+        return LIBMATTI_MC_VanillaModels_CUBE_ALL;
+    if (strcmp(modelId, "minecraft:block/cube") == 0 || strcmp(modelId, "block/cube") == 0)
+        return LIBMATTI_MC_VanillaModels_CUBE;
+    if (strcmp(modelId, "minecraft:block/stone") == 0 || strcmp(modelId, "block/stone") == 0)
+        return LIBMATTI_MC_VanillaModels_STONE;
+    if (strcmp(modelId, "minecraft:block/dirt") == 0 || strcmp(modelId, "block/dirt") == 0)
+        return LIBMATTI_MC_VanillaModels_DIRT;
+    return NULL;
+}
